8130721a394e04f5b5c9fce1ab385700e6a4cc3c
ahsanfirdaus1 <ahsanfirdaus1@gmail.com>
2024-01-28 13:43:37 +0700

    revisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -4,8 +4,11 @@ console.log(add(1,2));
 const substract = (a, b) => a -b;
 console.log(substract(5,3));
 
+<<<<<<< HEAD
 const multiply = (a,b) => a * b;
 console.log(multiply(3,5));
 
 const devide = (a,b) => a / b;
 console.log(devide(4,2));
+=======
+>>>>>>> parent of 439dcf1 (Menambahkan fitur perkalian)
